08ea487a680d8e542b417fb8ecd3acfee4f4b68d
alberto <albertohf91@gmail.com>
2021-01-07 18:17:21 -0300

    ProjectRouter

---
+++ src/components/NavBar.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import { ROUTES } from "../constants/routes"
+
+
+const NavBar = () => {
+    return (
+        <>
+            <ul>
+                <li>
+                    <Link to={ROUTES.HOME}>Home</Link>
+                </li>
+                <li>
+                    <Link to={ROUTES.ABOUT}>About</Link>
+                </li>
+                <li>
+                    <Link to={ROUTES.CONTACT}>Contact</Link>
+                </li>
+            </ul>
+        </>
+    );
+};
+export default NavBar;
---
+++ src/layout.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Navbar from "./components/NavBar"
+
+
+const Layout = (props) => {
+    return (
+        <>
+            <Navbar />
+            <div>{props.children}</div>
+        </>
+    );
+};
+export default Layout;
